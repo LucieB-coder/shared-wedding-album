58e9ccc7e5749e217cd883a8e1cbd85a008e4425
LucieB-coder <78735080+LucieB-coder@users.noreply.github.com>
2025-06-08 13:32:30 +0200

    Update ImageInput.tsx revert temp

--- src/components/form/ImageInput.tsx
+++ src/components/form/ImageInput.tsx
@@ -104,7 +104,7 @@ const ImageInput = (props: ImageInputProps) => {
                 type="file"
                 id="images"
                 onChange={handleImageChange}
-                accept="image/*, video/*"
+                accept="image/*"
                 multiple={true}
                 style={{display: "none"}}
                 className="bg-pink-200 w-20 h-20"
