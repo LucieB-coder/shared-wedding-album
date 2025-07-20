e11d5c41a2ae200395bbe1b8ada688a8d5a3964e
LucieB-coder <bedouretlucie@gmail.com>
2025-07-20 17:02:20 +0200

    ✨ Add delete button item button

--- src/api/images/mutations.ts
+++ src/api/images/mutations.ts
@@ -1,101 +1,193 @@
+import { useMutation } from "@tanstack/react-query";
 import imageCompression from "browser-image-compression";
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import {db, storage} from "../../firebaseConfig";
-import type {AlbumItem, ImageUrls} from "./types.ts";
-import {useMutation} from "@tanstack/react-query";
-import {addDoc, collection} from "firebase/firestore";
-import {v4 as uuid} from "uuid";
-import {queryClient} from "../../main.tsx";
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
+import {
+    deleteObject,
+    getDownloadURL,
+    ref,
+    uploadBytes,
+} from "firebase/storage";
+import { v4 as uuid } from "uuid";
+import { db, storage } from "../../firebaseConfig";
+import { queryClient } from "../../main.tsx";
+import type { AlbumItem, ImageUrls } from "./types.ts";
 
-export const uploadImageAndCompress = async (albumItem: CreateAlbumItem
+//#region - Creation
+
+export const uploadImageAndCompress = async (
+  albumItem: CreateAlbumItem
 ): Promise<ImageUrls> => {
-    try {
-        const uniqueName = `${albumItem.guestName}_${albumItem.createdAt}_${uuid()}`;
+  try {
+    const uniqueName = `${albumItem.guestName}_${
+      albumItem.createdAt
+    }_${uuid()}`;
 
-        const fileType = albumItem.file.type;
+    const fileType = albumItem.file.type;
 
-        if (fileType.startsWith("image/")) {
-            // === IMAGE ===
-            const thumbnail = await imageCompression(albumItem.file, {
-                maxWidthOrHeight: 300,
-                maxSizeMB: 0.5,
-                useWebWorker: true,
-            });
+    if (fileType.startsWith("image/")) {
+      // === IMAGE ===
+      const thumbnail = await imageCompression(albumItem.file, {
+        maxWidthOrHeight: 300,
+        maxSizeMB: 0.5,
+        useWebWorker: true,
+      });
 
-            const thumbRef = ref(storage, `images/thumbnails/${uniqueName}_thumb.jpg`);
-            const thumbSnap = await uploadBytes(thumbRef, thumbnail);
-            const thumbnailUrl = await getDownloadURL(thumbSnap.ref);
+      const thumbRef = ref(
+        storage,
+        `images/thumbnails/${uniqueName}_thumb.jpg`
+      );
+      const thumbSnap = await uploadBytes(thumbRef, thumbnail);
+      const thumbnailUrl = await getDownloadURL(thumbSnap.ref);
 
-            const compressed = await imageCompression(albumItem.file, {
-                maxWidthOrHeight: 1480,
-                maxSizeMB: 2,
-                useWebWorker: true,
-            });
+      const compressed = await imageCompression(albumItem.file, {
+        maxWidthOrHeight: 1480,
+        maxSizeMB: 2,
+        useWebWorker: true,
+      });
 
-            const compressedRef = ref(storage, `images/compressed/${uniqueName}_compressed.jpg`);
-            const compressedSnap = await uploadBytes(compressedRef, compressed);
-            const compressedUrl = await getDownloadURL(compressedSnap.ref);
+      const compressedRef = ref(
+        storage,
+        `images/compressed/${uniqueName}_compressed.jpg`
+      );
+      const compressedSnap = await uploadBytes(compressedRef, compressed);
+      const compressedUrl = await getDownloadURL(compressedSnap.ref);
 
-            const fullRef = ref(storage, `images/full/${uniqueName}_original.jpg`);
-            const fullSnap = await uploadBytes(fullRef, albumItem.file);
-            const fullUrl = await getDownloadURL(fullSnap.ref);
+      const fullRef = ref(storage, `images/full/${uniqueName}_original.jpg`);
+      const fullSnap = await uploadBytes(fullRef, albumItem.file);
+      const fullUrl = await getDownloadURL(fullSnap.ref);
 
-            return {
-                thumbnail: thumbnailUrl,
-                compressed: compressedUrl,
-                fullImage: fullUrl,
-            };
-        } else if (fileType.startsWith("video/")) {
-            // === VIDEO ===
-            const videoRef = ref(storage, `videos/${uniqueName}.${albumItem.file.name.split(".").pop()}`);
-            const videoSnap = await uploadBytes(videoRef, albumItem.file);
-            const videoUrl = await getDownloadURL(videoSnap.ref);
+      return {
+        thumbnail: thumbnailUrl,
+        compressed: compressedUrl,
+        fullImage: fullUrl,
+      };
+    } else if (fileType.startsWith("video/")) {
+      // === VIDEO ===
+      const videoRef = ref(
+        storage,
+        `videos/${uniqueName}.${albumItem.file.name.split(".").pop()}`
+      );
+      const videoSnap = await uploadBytes(videoRef, albumItem.file);
+      const videoUrl = await getDownloadURL(videoSnap.ref);
 
-            return {
-                fullImage: videoUrl,
-                compressed: videoUrl,
-                thumbnail: videoUrl,
-            };
-        } else {
-            throw new Error("Unsupported file type.");
-        }
-
-    } catch (error) {
-        //@ts-expect-error error is of type Error
-        throw new Error(`Upload failed: ${error.message || error.toString()}`);
+      return {
+        fullImage: videoUrl,
+        compressed: videoUrl,
+        thumbnail: videoUrl,
+      };
+    } else {
+      throw new Error("Unsupported file type.");
     }
+  } catch (error) {
+    //@ts-expect-error error is of type Error
+    throw new Error(`Upload failed: ${error.message || error.toString()}`);
+  }
 };
 
 type CreateAlbumItem = Omit<AlbumItem, "id" | "imageUrls"> & {
-    file: File
+  file: File;
 };
 
 export const useUploadImageMutation = () => {
-    return useMutation<AlbumItem, Error, CreateAlbumItem>({
-        mutationFn: async (albumItemData): Promise<AlbumItem> => {
-            try {
-                const imagesUrls = await uploadImageAndCompress(albumItemData);
-                const albumItem: Omit<AlbumItem, "id"> = {
-                    imageUrls: imagesUrls,
-                    moments: albumItemData.moments,
-                    createdAt: albumItemData.createdAt,
-                    guestName: albumItemData.guestName,
-                };
+  return useMutation<AlbumItem, Error, CreateAlbumItem>({
+    mutationFn: async (albumItemData): Promise<AlbumItem> => {
+      try {
+        const imagesUrls = await uploadImageAndCompress(albumItemData);
+        const albumItem: Omit<AlbumItem, "id"> = {
+          imageUrls: imagesUrls,
+          moments: albumItemData.moments,
+          createdAt: albumItemData.createdAt,
+          guestName: albumItemData.guestName,
+        };
 
-                const docRef = await addDoc(collection(db, "album_items"), albumItem);
+        const docRef = await addDoc(collection(db, "album_items"), albumItem);
 
-
-                return {
-                    id: docRef.id,
-                    ...albumItem
-                } as AlbumItem;
-            } catch (e) {
-                console.error(e);
-                throw (e);
-            }
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: [`album-items`]})
-        }
-    });
+        return {
+          id: docRef.id,
+          ...albumItem,
+        } as AlbumItem;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [`album-items`] });
+    },
+  });
 };
+
+//#endregion - Creation
+
+//#region - Deletion
+
+const extractImageNameFromUrl = (url: string) => {
+  // Split the url by "/"
+  const parts = url.split("/");
+
+  // The image name is the second last part of the url, and it's URL encoded
+  const encodedImageName = parts[parts.length - 1];
+
+  // Decode the image name
+  const decodedImageName = decodeURIComponent(encodedImageName);
+
+  // Remove the "images%2F" prefix and everything after the "?"
+  const imageName = decodedImageName.split("?")[0];
+
+  return imageName;
+};
+
+const deleteAlbumItem = async (albumItem: AlbumItem) => {
+  const guestName = localStorage.getItem("guestName") ?? "Inconnu";
+
+  if (
+    guestName !== "corinne" &&
+    guestName !== "frank" &&
+    guestName !== "lucie"
+  ) {
+    console.log(guestName);
+
+    throw new Error("Guest not allowed to delete items");
+  }
+
+  try {
+    const { imageUrls } = albumItem;
+
+    const storageRefs: string[] = [];
+
+    if (imageUrls.thumbnail)
+      storageRefs.push(extractImageNameFromUrl(imageUrls.thumbnail));
+    if (imageUrls.compressed)
+      storageRefs.push(extractImageNameFromUrl(imageUrls.compressed));
+
+    await Promise.all(
+      storageRefs.map(async (path) => {
+        const fileRef = ref(storage, path);
+        await deleteObject(fileRef).catch((err) => {
+          console.warn(
+            "Erreur lors de la suppression d’un fichier:",
+            path,
+            err.message
+          );
+        });
+      })
+    );
+    await deleteDoc(doc(db, "album_items", albumItem.id));
+  } catch (error) {
+    throw new Error(
+      //@ts-expect-error
+      `Échec de la suppression : ${error.message || error.toString()}`
+    );
+  }
+};
+
+export const useDeleteAlbumItemMutation = () => {
+  return useMutation<void, Error, AlbumItem>({
+    mutationFn: deleteAlbumItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["album-items"] });
+    },
+  });
+};
+
+//#endregion - Deletion
--- src/components/galleryModal.tsx
+++ src/components/galleryModal.tsx
@@ -1,135 +1,188 @@
-import {ChevronLeft, ChevronRight} from "lucide-react";
-import {useEffect, useState} from "react";
-import {Carousel, type CarouselApi, CarouselContent, CarouselItem,} from "./Carousel";
-import {type AlbumItem, MOMENTS} from "../api/images/types.ts";
-import {Button} from "./Button.tsx";
-import {Dialog, DialogContent} from "./Dialog";
+import { ChevronLeft, ChevronRight, TrashIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useDeleteAlbumItemMutation } from "../api/images/mutations.ts";
+import { type AlbumItem, MOMENTS } from "../api/images/types.ts";
+import { Button } from "./Button.tsx";
+import {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "./Carousel";
+import { Dialog, DialogContent } from "./Dialog";
 
 export const PhotoGalleryModal = ({
-                                      open,
-                                      onOpen,
-                                      photos,
-                                      selectedPhotoIndex,
-                                      hasNextPage,
-                                      fetchNextPage,
-                                  }: {
-    open: boolean;
-    onOpen: (open: boolean) => void;
-    photos: AlbumItem[];
-    selectedPhotoIndex: number | null;
-    hasNextPage: boolean;
-    fetchNextPage: () => void;
+  open,
+  onOpen,
+  photos,
+  selectedPhotoIndex,
+  hasNextPage,
+  fetchNextPage,
+}: {
+  open: boolean;
+  onOpen: (open: boolean) => void;
+  photos: AlbumItem[];
+  selectedPhotoIndex: number | null;
+  hasNextPage: boolean;
+  fetchNextPage: () => void;
 }) => {
-    const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+  const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+  const deleteItem = useDeleteAlbumItemMutation();
 
-    useEffect(() => {
-        if (carouselApi && selectedPhotoIndex !== null) {
-            carouselApi.scrollTo(selectedPhotoIndex, true);
-        }
-    }, [carouselApi, selectedPhotoIndex]);
+  useEffect(() => {
+    if (carouselApi && selectedPhotoIndex !== null) {
+      carouselApi.scrollTo(selectedPhotoIndex, true);
+    }
+  }, [carouselApi, selectedPhotoIndex]);
 
-    useEffect(() => {
-        if (!carouselApi || selectedPhotoIndex === null) return;
+  useEffect(() => {
+    if (!carouselApi || selectedPhotoIndex === null) return;
 
-        const isLastPhoto = selectedPhotoIndex === photos.length - 1;
-        if (isLastPhoto && hasNextPage) {
-            fetchNextPage();
-        }
-    }, [selectedPhotoIndex, photos.length, hasNextPage, fetchNextPage, carouselApi]);
+    const isLastPhoto = selectedPhotoIndex === photos.length - 1;
+    if (isLastPhoto && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [
+    selectedPhotoIndex,
+    photos.length,
+    hasNextPage,
+    fetchNextPage,
+    carouselApi,
+  ]);
 
-    const [currentIndex, setCurrentIndex] = useState<number>(selectedPhotoIndex ?? 0);
-    useEffect(() => {
-        const isLastPhoto = currentIndex === photos.length - 1;
-        if (isLastPhoto && hasNextPage) {
-            fetchNextPage();
-        }
-    }, [currentIndex, photos.length, hasNextPage, fetchNextPage]);
+  const [currentIndex, setCurrentIndex] = useState<number>(
+    selectedPhotoIndex ?? 0
+  );
+  useEffect(() => {
+    const isLastPhoto = currentIndex === photos.length - 1;
+    if (isLastPhoto && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [currentIndex, photos.length, hasNextPage, fetchNextPage]);
 
-    useEffect(() => {
-        if (carouselApi) {
-            carouselApi.on("select", () => {
-                setCurrentIndex(carouselApi.selectedScrollSnap());
-            });
-        }
-    }, [carouselApi]);
+  useEffect(() => {
+    if (carouselApi) {
+      carouselApi.on("select", () => {
+        setCurrentIndex(carouselApi.selectedScrollSnap());
+      });
+    }
+  }, [carouselApi]);
 
+  const handlePrevious = () => {
+    carouselApi?.scrollPrev();
+  };
 
-    const handlePrevious = () => {
-        carouselApi?.scrollPrev();
-    };
+  const handleNext = () => {
+    carouselApi?.scrollNext();
+  };
 
-    const handleNext = () => {
-        carouselApi?.scrollNext();
-    };
+  const onDeleteItem = async ({ item }: { item: AlbumItem }) => {
+    try {
+      await deleteItem.mutateAsync(item);
+      handlePrevious();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
-        <Dialog open={open} onOpenChange={onOpen}>
-            <DialogContent
-                className="w-[100%] text-white overflow-hidden flex items-center justify-center focus:outline-none focus:ring-0">
-                {photos.length > 0 && selectedPhotoIndex !== null && (
-                    <div className="flex w-screen max-h-[80vh] h-full items-center justify-center space-x-3">
-                        <Button
-                            onClick={handlePrevious}
-                            className="bg-transparent rounded-full hover:bg-surface/40 hidden md:flex"
-                        >
-                            <ChevronLeft className="h-8 w-auto"/>
-                        </Button>
-                        <Carousel
-                            opts={{loop: false}}
-                            setApi={setCarouselApi}
-                            className="flex-1 max-w-[100%] pl-3 items-center justify-center"
-                        >
-                            <CarouselContent>
-                                {photos.map((photo: AlbumItem) => (
-                                    <CarouselItem
-                                        key={photo.id}
-                                        className="relative flex flex-col gap-2 h-[90%]"
-                                    >
-                                        <div className="flex flex-col h-full w-full">
-                                            <div className="flex-1 flex items-center justify-center overflow-hidden">
-                                                {photo.imageUrls.compressed?.endsWith(".mp4") ||
-                                                photo.imageUrls.compressed?.includes("video") ? (
-                                                    <video
-                                                        controls
-                                                        height={"90%"}
-                                                        className="max-h-[80vh] max-w-full object-contain"
-                                                    >
-                                                        <source src={photo.imageUrls.compressed} type="video/mp4"/>
-                                                        Your browser does not support the video tag.
-                                                    </video>
-                                                ) : (
-                                                    <img
-                                                        alt={`Photo ${photo.id}`}
-                                                        src={photo.imageUrls.compressed}
-                                                        height={"90%"}
-                                                        className="max-h-[80vh] max-w-full object-contain"
-                                                    />
-                                                )}
-
-                                            </div>
-                                            <div className="flex justify-center w-full p-2 z-50">
-                                                <div className="text-center text-primary-200">
+  return (
+    <Dialog open={open} onOpenChange={onOpen}>
+      <DialogContent className="w-[100%] text-white overflow-hidden flex items-center justify-center focus:outline-none focus:ring-0">
+        {photos.length > 0 && selectedPhotoIndex !== null && (
+          <div className="flex w-screen max-h-[80vh] h-full items-center justify-center space-x-3">
+            <Button
+              onClick={handlePrevious}
+              className="bg-transparent rounded-full hover:bg-surface/40 hidden md:flex"
+            >
+              <ChevronLeft className="h-8 w-auto" />
+            </Button>
+            <Carousel
+              opts={{ loop: false }}
+              setApi={setCarouselApi}
+              className="flex-1 max-w-[100%] pl-3 items-center justify-center"
+            >
+              <CarouselContent>
+                {photos.map((item: AlbumItem) => (
+                  <CarouselItem
+                    key={item.id}
+                    className="relative flex flex-col gap-2 h-[90%]"
+                  >
+                    <div className="flex flex-col h-full w-full">
+                      <div className="flex-1 flex items-center justify-center overflow-hidden">
+                        {item.imageUrls.compressed?.endsWith(".mp4") ||
+                        item.imageUrls.compressed?.includes("video") ? (
+                          <video
+                            controls
+                            height={"90%"}
+                            className="max-h-[80vh] max-w-full object-contain"
+                          >
+                            <source
+                              src={item.imageUrls.compressed}
+                              type="video/mp4"
+                            />
+                            Your browser does not support the video tag.
+                          </video>
+                        ) : (
+                          <img
+                            alt={`Photo ${item.id}`}
+                            src={item.imageUrls.compressed}
+                            height={"90%"}
+                            className="max-h-[80vh] max-w-full object-contain"
+                          />
+                        )}
+                      </div>
+                      <div className="flex justify-center w-full p-2 z-50 gap-3 items-center">
+                        <div className="text-center text-primary-200">
                           <span className="italic underline">
                             Photo prise par :
                           </span>
-                                                    <span className={"capitalize"}>{" "}{photo.guestName}</span>
-                                                    {photo.moments?.map(m => (<p key={m}>{MOMENTS[m]}</p>))}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CarouselItem>
-                                ))}
-                            </CarouselContent>
-                        </Carousel>
-                        <Button
-                            onClick={handleNext}
-                            className="bg-transparent rounded-full hover:bg-surface/40 hidden md:flex"
-                        >
-                            <ChevronRight className="h-8 w-auto"/>
-                        </Button>
+                          <span className={"capitalize"}>
+                            {" "}
+                            {item.guestName}
+                          </span>
+                          {item.moments?.map((m) => (
+                            <p key={m}>{MOMENTS[m]}</p>
+                          ))}
+                        </div>
+                        <DeleteItemButton
+                          onDelete={() => onDeleteItem({ item })}
+                          disabled={deleteItem.isPending}
+                        />
+                      </div>
                     </div>
-                )}
-            </DialogContent>
-        </Dialog>
-    );
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+            <Button
+              onClick={handleNext}
+              className="bg-transparent rounded-full hover:bg-surface/40 hidden md:flex"
+            >
+              <ChevronRight className="h-8 w-auto" />
+            </Button>
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
 };
+
+function DeleteItemButton({
+  onDelete,
+  disabled,
+}: {
+  onDelete: () => void;
+  disabled: boolean;
+}) {
+  const guestName = localStorage.getItem("guestName") ?? "Inconnu";
+  if (guestName === "frank" || guestName === "corinne")
+    return (
+      <Button
+        className="bg-red-700 text-white/90 hover:bg-red-500"
+        onClick={onDelete}
+        disabled={disabled}
+      >
+        <TrashIcon className="size-5 shrink-0" />
+      </Button>
+    );
+}
